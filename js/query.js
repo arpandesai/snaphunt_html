e12fc74b722f1a1c5f00c5815a4ec1629fac81af
Nisarg Joshi <nisarg@vevento.com>
2017-01-24 17:24:45 +0530

    delete blocks and clione particular "Achievements". Review it so if it correct we can implement for all blocks.

--- js/query.js
+++ js/query.js
@@ -1,13 +1,65 @@
 $(document).ready(function() {
 	$('.fixed_textarea').scroll(function () {
-    $(this).css("overflow", "hidden");      /* for the mozilla browser problem */
-    $(this).animate({scrollTop: $(this).outerHeight()});
-    while ($(this).scrollTop() > 0) {       /* for the copy and paste case */               
-        lines=$(this).val().slice(0,-1);
-        $(this).val(lines);
-    }
-    $(this).css("overflow", "auto");        /* For the mozilla browser problem */
+	    $(this).css("overflow", "hidden");      /* for the mozilla browser problem */
+	    $(this).animate({scrollTop: $(this).outerHeight()});
+	    while ($(this).scrollTop() > 0) {       /* for the copy and paste case */               
+	        lines=$(this).val().slice(0,-1);
+	        $(this).val(lines);
+	    }
+	    $(this).css("overflow", "auto");        /* For the mozilla browser problem */
+	});
+
+	$(document).on("click",".color_change_on_click",function(){
+		$(".color_change_on_click").css("background-color","#B2ECDD");
+		$(".icon_color").css("background-color","#B2ECDD");
+		$(".user_photo").css("opacity","0.2");
+		$(this).css("background-color","white");
+		$(this).find("i").css("background-color","white");
+		$(this).find(".user_photo").css("opacity","10");
+	});
+
+	$(document).on('click','.del_achivement',function(){
+		$("#"+$("#delete_div").val()).remove();
+	});
+
+	deleteDivById();
+
+	$('[data-toggle="popover"]').popover({
+        placement : 'top',
+        html : true,
+        content : '<i class="fa fa-plus" id="add_achivement" aria-hidden="true"></i><i class="fa fa-trash del_achivement" rel="" aria-hidden="true"></i>'
+    });
+
+    $(document).on("click", ".popover .close" , function(){
+        $(this).parents(".popover").popover('hide');
+    });
 });
-	
-	$('[data-toggle="popover"]').popover();
+
+function deleteDivById(){
+	$('[data-toggle="popover"]').on('show.bs.popover', function () {
+		if($(this).hasClass('div_heading')==true){
+			$("#delete_div").val($(this).parent().attr('id'));
+		}
+		else{
+			$("#delete_div").val(this.id);
+		}	
+
+	});
+}
+
+
+$(function(){
+  var count = 2;
+  $(document).on('click','#add_achivement',function(){
+  	$(".color_change_on_click").css("background-color","#B2ECDD");
+  	$(".icon_color").css("background-color","#B2ECDD");
+    $('.achivement_main_div').append('<div id="achivement_div'+count+'" class="input-group color_change_on_click" data-trigger="focus" data-toggle="popover"><i class="input-group-addon fa fa-trophy fa-2x icon_color" id="basic-addon1" aria-hidden="true"></i><textarea placeholder="Achivements Details" class="form-control background_transparent fixed_textarea" rows="2" required ></textarea></div>');
+    $('#achivement_div'+count).popover({
+        placement : 'top',
+        html : true,
+        content : '<i class="fa fa-plus" id="add_achivement" aria-hidden="true"></i><i class="fa fa-trash del_achivement" aria-hidden="true"></i>'
+    });
+    deleteDivById();
+    count++;
+  });
 });

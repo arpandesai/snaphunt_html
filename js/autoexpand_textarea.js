d3843b4bc6fe3e6e53decf30d18a2d59d7e351f9
Nisarg Joshi <nisarg@vevento.com>
2017-02-21 16:39:41 +0530

    textarea expand done

---
+++ js/autoexpand_textarea.js
@@ -0,0 +1,15 @@
+$(document)
+    .on('focus.autoExpand', 'textarea.autoExpand', function(){
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.autoExpand', 'textarea.autoExpand', function(){
+        var minRows = this.getAttribute('data-min-rows')|0, rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 20);
+        this.rows = minRows + rows;
+    });
+
+    
